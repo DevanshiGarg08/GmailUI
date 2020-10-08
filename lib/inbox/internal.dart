99ff3f82bbb08fa91612e5def627602ee173d55e
Devanshi Garg <devanshig@geekyants.com>
2020-10-08 14:23:28 +0530

    Updated

--- lib/inbox/internal.dart
+++ lib/inbox/internal.dart
@@ -1,313 +1,211 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/model/mails.dart';
 
-
-// class AppInternal extends StatefulWidget
-// {
-//    String name;
-//    String title;
-//    String time;
-//   AppInternal(this.name,this.title,this.time);
-//   @override
-//   Internal createState() {
-//     // TODO: implement createState
-//     return Internal(name,title,time);
-//     throw UnimplementedError();
-//   }
-  
-// }
-
-class Internal extends StatelessWidget
-{
-
-
+class Internal extends StatelessWidget {
   String name;
-   String title;
-   String time;
-   Color color;
-   String heading;
-   String para;
-   Internal(this.name,this.title, this.time,this.color,this.heading,this.para);
+  String title;
+  String time;
+  Color color;
+  String heading;
+  String para;
+  Internal(
+      this.name, this.title, this.time, this.color, this.heading, this.para);
   @override
   Widget build(BuildContext context) {
     final String x = ModalRoute.of(context).settings.arguments;
-    
+
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.arrow_back),
-        onPressed: ()
-         {
-           Navigator.pop(context);
-         }
-         ),
-        //  title: Text(""),
-              actions: <Widget>[
-    Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: Icon(
-          Icons.archive,
-          size: 26.0,
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          //  title: Text(""),
+          actions: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.archive,
+                    size: 26.0,
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.delete,
+                    size: 26.0,
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.mail,
+                    size: 26.0,
+                  ),
+                )),
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.more_vert,
+                    size: 26.0,
+                  ),
+                ))
+          ],
         ),
-      )
-    ),
-
-    Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: Icon(
-          Icons.delete,
-          size: 26.0,
-        ),
-      )
-    ),
-
-    Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: Icon(
-          Icons.mail,
-          size: 26.0,
-        ),
-      )
-    ),
-
-    Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: GestureDetector(
-        onTap: () {},
-        child: Icon(
-          Icons.more_vert,
-          size: 26.0,
-        ),
-      )
-    )
-   
-  ],
-      ),
-
-  //     body: ListView.separated(
-  //       itemCount: 1,
-  //       itemBuilder: (context,index)
-  //       {
-  //         return ListTile(
-  //            title: Text(name,
-  //        style: TextStyle(
-  //          fontSize:20.0,
-  //          fontWeight:FontWeight.w700,
-  //          fontFamily: 'Poppins'
-  //        ),
-  //        )
-  //         );
-          
-         
-  //       },
-  //        separatorBuilder: (context, index) {
-  //   return Divider();
-  // },
-  //       ),
-
-  body: SingleChildScrollView(
-    child:Column(
-    children:[
-      ListTile(
-         title: Text(title,
-         style: TextStyle(
-           fontSize:18.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins'
-         ),
-         ),
-         trailing: Icon(Icons.star_border),
-         
-      ),
-      Container(
-        child: FlatButton(onPressed: ()
-      {
-
-      }, 
-      child: Text("Inbox",
-      style: TextStyle(
-        fontSize: 12.0
-      ),),
-      color: Colors.grey[300],
-      ),
-      width: 65.0,
-      height: 22.0,
-      margin: EdgeInsets.only(right:310.0),
-      ),
-      Divider(),
-
-      ListTile(
-        isThreeLine: true,
-         
-          leading: CircleAvatar(
-            backgroundColor:color,
-            child:Text(name.substring(0,1),
-            style: TextStyle(
-              fontSize:25.0,
-              color:Colors.white
-            ),),
-            radius: 26,
-            
-          ),
-         title: Text(name,
-       
-         style: TextStyle(
-           fontSize:18.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           
-         ),
-         ),
-         
-         subtitle: Column(
-
-                children: <Widget>[
-                  Text("to me",
-                  textAlign: TextAlign.left,
-                    // overflow: TextOverflow.ellipsis,
-                    // softWrap: false,
-                  style: TextStyle(
-                     fontSize:18.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           color: Colors.grey
-                  ),
-                  ),
-                  
-                  Row(
-                    children:[
-                    Text("${time}",
-                  // overflow: TextOverflow.ellipsis,
-                  //   softWrap: false,
-                  style: TextStyle(
-                     fontSize:16.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           color: Colors.grey
-                  ),
-                  ),
-                 SizedBox(width:8.0),
-                  Text("View Details",
-                  // overflow: TextOverflow.ellipsis,
-                  //   softWrap: false,
-                  style: TextStyle(
-                     fontSize:16.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           color: Colors.blue
-                  ),
-                  )
-                    ]
-                  )
-                  
-         
-                  
-                ],
-                crossAxisAlignment: CrossAxisAlignment.stretch
+        body: SingleChildScrollView(
+          child: Column(children: [
+            ListTile(
+              title: Text(
+                title,
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Poppins'),
               ),
-
-              trailing:  Wrap(
-    spacing: 12, // space between two icons
-    children: <Widget>[
-      Icon(Icons.reply),// icon-1
-      Icon(Icons.more_vert), // icon-2
-    ],
-  ),
-               
-                  
-
-                  
-                  
-               
-      ),
-
-      SizedBox(height:10.0),
-
-      Row(
-                    children:[
-                      SizedBox(width:100.0),
-                    Text("To view this email as a webpage,",
+              trailing: Icon(Icons.star_border),
+            ),
+            Container(
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Inbox",
+                  style: TextStyle(fontSize: 12.0),
+                ),
+                color: Colors.grey[300],
+              ),
+              width: 65.0,
+              height: 22.0,
+              margin: EdgeInsets.only(right: 310.0),
+            ),
+            Divider(),
+            ListTile(
+              isThreeLine: true,
+              leading: CircleAvatar(
+                backgroundColor: color,
+                child: Text(
+                  name.substring(0, 1),
+                  style: TextStyle(fontSize: 25.0, color: Colors.white),
+                ),
+                radius: 26,
+              ),
+              title: Text(
+                name,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+              subtitle: Column(children: <Widget>[
+                Text(
+                  "to me",
+                  textAlign: TextAlign.left,
                   // overflow: TextOverflow.ellipsis,
-                  //   softWrap: false,
+                  // softWrap: false,
                   style: TextStyle(
-                     fontSize:12.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           color: Colors.black
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Poppins',
+                      color: Colors.grey),
+                ),
+                Row(children: [
+                  Text(
+                    "${time}",
+                    // overflow: TextOverflow.ellipsis,
+                    //   softWrap: false,
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins',
+                        color: Colors.grey),
                   ),
-                  ),
-                 SizedBox(width:3.0),
-                  Text("Click here",
-                  // overflow: TextOverflow.ellipsis,
-                  //   softWrap: false,
-                  style: TextStyle(
-                     fontSize:12.0,
-           fontWeight:FontWeight.w500,
-           fontFamily: 'Poppins',
-           color: Colors.blue
-                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    "View Details",
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins',
+                        color: Colors.blue),
                   )
-                    ]
+                ])
+              ], crossAxisAlignment: CrossAxisAlignment.stretch),
+              trailing: Wrap(
+                spacing: 12,
+                children: <Widget>[
+                  Icon(Icons.reply),
+                  Icon(Icons.more_vert),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Row(children: [
+              SizedBox(width: 100.0),
+              Text(
+                "To view this email as a webpage,",
+                style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Poppins',
+                    color: Colors.black),
+              ),
+              SizedBox(width: 3.0),
+              Text(
+                "Click here",
+                style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Poppins',
+                    color: Colors.blue),
+              )
+            ]),
+            SizedBox(height: 8.0),
+            Alexa(),
+            SizedBox(height: 8.0),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    heading,
+                    style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Poppins'),
                   ),
-                  
-                  SizedBox(height:8.0),
-
-                  // Container(
-                  //   width:400.0,
-                  //   height:400.0,
-                  //   color:Colors.yellow
-                  // ),
-
-                Alexa(),
-
-                  SizedBox(height:8.0),
-
-                  Container(
-                    child:Column(children: [
-                      Text(heading,
-                      style: TextStyle(
-                        fontSize:22.0,
-                        fontWeight:FontWeight.w500,
-                        fontFamily:'Poppins'
-                      ),),
-                      Text(para,
-                      style: TextStyle(
-                        fontSize:18.0,
-                        fontWeight:FontWeight.w300,
-                        fontFamily:'Poppins'
-                      ),)
-                    ],),
-                    margin: EdgeInsets.all(10.0),
+                  Text(
+                    para,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: 'Poppins'),
                   )
-           
-    ]
-  ),
-  )
-  
-    );
+                ],
+              ),
+              margin: EdgeInsets.all(10.0),
+            )
+          ]),
+        ));
     throw UnimplementedError();
   }
-
-  
-  
 }
 
-class Alexa extends StatelessWidget
-{
+class Alexa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
-    AssetImage asset=AssetImage('images/alexa.jpg');
-    Image image=Image(image: asset);
-    return Container(child:image);
+    AssetImage asset = AssetImage('images/alexa.jpg');
+    Image image = Image(image: asset);
+    return Container(child: image);
     throw UnimplementedError();
   }
-  
 }
--- lib/model/mails.dart
+++ lib/model/mails.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Mails
-{
- List<String> main=[
-  "Amazon Alexa Team",
-  "Dribbble",
-  "Google Local Guides",
-  "Udacity India",
-  "Hacker Earth",
-  "Amazon Web Services",
-  "Oracle",
-  "Facebook Team",
-  "MongoDB "
+class Mails {
+  List<String> main = [
+    "Amazon Alexa Team",
+    "Dribbble",
+    "Google Local Guides",
+    "Udacity India",
+    "Hacker Earth",
+    "Amazon Web Services",
+    "Oracle",
+    "Facebook Team",
+    "MongoDB "
   ];
 
- List<String> title=[
+  List<String> title = [
     "[REMINDER] This October,Build Alexa skills and Earn Sony PlayStation 4",
     "Overtime from mastering skeumorphic designs",
     "Your Forum for Google news, travel,food,etc",
@@ -23,10 +22,10 @@ class Mails
     "Recent Announcements from AWS",
     "San Francisco is coming to Dubai!! Register Now",
     "[1 DAY]- Upgrade your paycheck with Udacity",
-     "Are you ready to test your skills"
+    "Are you ready to test your skills"
   ];
 
-  List<String> subtitle=[
+  List<String> subtitle = [
     "[REMIND..] This October,Build Alexa skills and Earn money",
     "Overtime from mastering skeumorphic designs",
     "Your Forum for Google news, travel,food,etc",
@@ -35,59 +34,54 @@ class Mails
     "Recent Announcements from AWS",
     "San Francisco is coming to Dubai!! Register Now",
     "[1 DAY]- Upgrade your paycheck with Udacity",
-     "Are you ready to test your skills"
+    "Are you ready to test your skills"
   ];
 
-  List<String> time=[
-  "1:28 AM",
-  "6:28 PM",
-  "Oct 3",
-  "Oct 1",
-  "Sep 29",
-  "Sep 29",
-  "Sep 28",
-  "Sep 26",
-  "Sep 25"
-];
+  List<String> time = [
+    "1:28 AM",
+    "6:28 PM",
+    "Oct 3",
+    "Oct 1",
+    "Sep 29",
+    "Sep 29",
+    "Sep 28",
+    "Sep 26",
+    "Sep 25"
+  ];
 
- List<Color> colors = [
-  Colors.grey,
-  Colors.purple,
-  Colors.grey[600],
-  Colors.brown,
-  Colors.orange,
-  Colors.green,
-  Colors.pink,
-  Colors.blue,
-  Colors.indigo
+  List<Color> colors = [
+    Colors.grey,
+    Colors.purple,
+    Colors.grey[600],
+    Colors.brown,
+    Colors.orange,
+    Colors.green,
+    Colors.pink,
+    Colors.blue,
+    Colors.indigo
+  ];
 
+  List<String> heading = [
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+    "Hurry! Only a few days left for the Alexa Developer festive offer",
+  ];
 
-];
-
-List<String> heading=[
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-  "Hurry! Only a few days left for the Alexa Developer festive offer",
-];
-
-List<String> para=[
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-"Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
-];
-
-
+  List<String> para = [
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+    "Publish a skill in October and get a limited-edition Alexa developer t-shirt. Continue to publish 3 or more skills this month and you will receive an Echo Dot in addition to the t-shirt",
+  ];
 }
- 
